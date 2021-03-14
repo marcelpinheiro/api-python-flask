558cc7c015d509d62ac79720b7979bbabbef7237
Marcel Borges Pinheiro <marcel.pinheiro@nuvem.net>
2021-03-14 17:30:17 -0400

    up

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask, g
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 
-
-
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -21,9 +18,9 @@ def get_arguments():
 def general():
         args = get_arguments()
         data = pd.read_csv('Revenue_Analysis_Test_Data.csv')  # read CSV
-        data = data.query("MEMBER_ID ==" + args['member_id'])
-        data = data.to_dict()  # convert dataframe to dictionary
-        return {'data': data}, 200  # return data and 200 OK code
+        if args.get('member_id') != None:
+                data = data.query("MEMBER_ID ==" + args['member_id'])
+        return data.to_json(), 200  # return data and 200 OK code
 pass
 
 @app.route('/totalwin',methods=["GET"])
@@ -35,20 +32,27 @@ def totalwin():
 
         if args.get('month') != None:
                 data = data[ data['ACTIVITY_YEAR_MONTH'].astype(str).str.endswith(args['month']) ]
+        if args.get('game_id') != None:
+                data = data.query("GAME_ID ==" + args['game_id'])
         
-        result = float(data['WIN_AMOUNT'].sum())
-        
-        return {'data': result}, 200  # return data and 200 OK code
+        data = data[['MEMBER_ID','WIN_AMOUNT']].groupby('MEMBER_ID').sum('WIN_AMOUNT')
+               
+        return data.to_json(), 200  # return data and 200 OK code
 pass
 
-@app.route('/totalwageramount',methods=["GET"])
+@app.route('/wageramount',methods=["GET"])
 def totalwageramount():
         args = get_arguments()
         data = pd.read_csv('Revenue_Analysis_Test_Data.csv')  # read CSV
         data = data.query("MEMBER_ID ==" + args['member_id'])
-        result = float(data['WAGER_AMOUNT'].sum())
-        # g.data = result  # convert dataframe to dictionary
-        return {'data': result}, 200  # return data and 200 OK code
+        if args.get('month') != None:
+                data = data[ data['ACTIVITY_YEAR_MONTH'].astype(str).str.endswith(args['month']) ]
+        if args.get('game_id') != None:
+                data = data.query("GAME_ID ==" + args['game_id'])
+
+        data = data[['MEMBER_ID','WAGER_AMOUNT']].groupby('MEMBER_ID').sum('WAGER_AMOUNT')        
+        
+        return data.to_json(), 200  # return data and 200 OK code
 pass
 
 @app.route("/wagernumber",methods=["GET"])
@@ -56,9 +60,12 @@ def wagernumber():
         args = get_arguments()
         data = pd.read_csv('Revenue_Analysis_Test_Data.csv')  # read CSV
         data = data.query("MEMBER_ID ==" + args['member_id'])
-        result = int(data['NUMBER_OF_WAGERS'].sum())
-        # data = result  # convert dataframe to dictionary
-        return {'data': result}, 200  # return data and 200 OK code
+        if args.get('month') != None:
+                data = data[ data['ACTIVITY_YEAR_MONTH'].astype(str).str.endswith(args['month']) ]
+        if args.get('game_id') != None:
+                data = data.query("GAME_ID ==" + args['game_id'])
+        data = data[['MEMBER_ID','NUMBER_OF_WAGERS']].groupby('MEMBER_ID').sum('NUMBER_OF_WAGERS')                        
+        return data.to_json(), 200  # return data and 200 OK code
 pass
 
     
